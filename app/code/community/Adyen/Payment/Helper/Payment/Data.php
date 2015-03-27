4933d7d50471df69ea2a60a182f79fd7acf976ca
Paul Hachmang <paul@h-o.nl>
2015-03-27 21:43:25 +0100

    Added comment where the Adyen_Payment_Helper_Payment_Data class is for.

--- app/code/community/Adyen/Payment/Helper/Payment/Data.php
+++ app/code/community/Adyen/Payment/Helper/Payment/Data.php
@@ -17,7 +17,14 @@
  * @copyright	Copyright (c) 2011 Adyen (http://www.adyen.com)
  * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
- 
+
+
+
+/**
+ * Because of the dynamic payment methods used we rewrite the Payment helper so we always get the right payment methods
+ * back.
+ * Class Adyen_Payment_Helper_Payment_Data
+ */
 class Adyen_Payment_Helper_Payment_Data extends Mage_Payment_Helper_Data {
 
     /**
