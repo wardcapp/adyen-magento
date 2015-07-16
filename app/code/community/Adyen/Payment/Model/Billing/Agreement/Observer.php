19ad5c828dd25da07543b1ed069b5e9d98e91ec7
rikterbeek <rikterbeek@me.com>
2015-07-16 13:06:37 +0200

    use adyen description

--- app/code/community/Adyen/Payment/Model/Billing/Agreement/Observer.php
+++ app/code/community/Adyen/Payment/Model/Billing/Agreement/Observer.php
@@ -1,22 +1,28 @@
 <?php
 /**
- * Adyen_Payment
+ * Adyen Payment Module
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the H&O Commercial License
- * that is bundled with this package in the file LICENSE_HO.txt.
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * http://www.h-o.nl/license
+ * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to info@h-o.com so we can send you a copy immediately.
+ * to license@magentocommerce.com so we can send you a copy immediately.
  *
- * @category  Adyen
- * @package   Adyen_Payment
- * @author    Paul Hachmang – H&O <info@h-o.nl>
- * @copyright 2015 Copyright © H&O (http://www.h-o.nl/)
- * @license   H&O Commercial License (http://www.h-o.nl/license)
+ * @category	Adyen
+ * @package	Adyen_Payment
+ * @copyright	Copyright (c) 2011 Adyen (http://www.adyen.com)
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+/**
+ * @category   Payment Gateway
+ * @package    Adyen_Payment
+ * @author     Adyen
+ * @property   Adyen B.V
+ * @copyright  Copyright (c) 2014 Adyen BV (http://www.adyen.com)
  */
  
 class Adyen_Payment_Model_Billing_Agreement_Observer
