587b5e662de0a4c03f02ab97256314ded9b65829
Nuno Gato <nuno@robosavvy.com>
2016-01-22 14:20:19 +0000

    updated field name to reflect actual action
    https://github.com/Adyen/magento/issues/515

--- app/code/community/Adyen/Payment/Model/Observer.php
+++ app/code/community/Adyen/Payment/Model/Observer.php
@@ -459,7 +459,7 @@ class Adyen_Payment_Model_Observer {
         /** @var Mage_Sales_Model_Order $order */
         $order = $payment->getOrder();
         
-        $autoRefund = $adyenHelper->getConfigData('autorefundoncreditmemo', 'adyen_abstract', $order->getStoreId());
+        $autoRefund = $adyenHelper->getConfigData('autorefundoncancel', 'adyen_abstract', $order->getStoreId());
 
         if($this->isPaymentMethodAdyen($order) && $autoRefund) {
             $pspReference = Mage::getModel('adyen/event')->getOriginalPspReference($order->getIncrementId());
