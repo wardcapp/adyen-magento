889212074227098bcfe0204c8f421bbe69d10330
Rik ter Beek <rikterbeek@me.com>
2016-07-18 14:48:37 +0200

    #629 add input field for encrypted data for oneclick method so at all times the encrypted key is send with the request

--- app/code/community/Adyen/Payment/Model/Adyen/Oneclick.php
+++ app/code/community/Adyen/Payment/Model/Adyen/Oneclick.php
@@ -97,7 +97,9 @@ class Adyen_Payment_Model_Adyen_Oneclick extends Adyen_Payment_Model_Adyen_Cc {
         $info->setCcType($ccType);
 
         if ($this->isCseEnabled()) {
-            $info->setAdditionalInformation('encrypted_data', $data->getEncryptedDataOneclick());
+            $method = $this->getCode();
+            $encryptedData = $data->getData('encrypted_data_'.$method);
+            $info->setAdditionalInformation('encrypted_data', $encryptedData);
         } else {
 
             // check if expiry month and year is changed
