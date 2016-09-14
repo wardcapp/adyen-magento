335299a31a40b71a5af5f9af46f7b0e160669e0e
Sander Mangel <sander@sandermangel.nl>
2016-09-14 16:18:03 +0200

    [TASK] persist adyen_pre_order attribute in quote_item and order_item table

--- app/code/community/Adyen/Payment/Model/Observers/Quote.php
+++ app/code/community/Adyen/Payment/Model/Observers/Quote.php
@@ -29,30 +29,24 @@ class Adyen_Payment_Model_Observers_Quote
      * call all observer methods listening to the sales_quote_item_set_product event
      *
      * @param Varien_Event_Observer $observer
-     *
-     * @return $this
      */
     public function salesQuoteItemSetProduct(Varien_Event_Observer $observer)
     {
         $this->_salesQuoteItemSetAttributes($observer);
-
-        return $this;
     }
 
     /**
      * set the quote item values based on product attribute values
      *
      * @param Varien_Event_Observer $observer
-     *
-     * @return $this
      */
-    protected function _salesQuoteItemSetIsSampleAttribute(Varien_Event_Observer $observer)
+    protected function _salesQuoteItemSetAttributes(Varien_Event_Observer $observer)
     {
         /** @var Mage_Sales_Model_Quote_Item $quoteItem */
         $quoteItem = $observer->getQuoteItem();
         /** @var Mage_Catalog_Model_Product $product */
         $product = $observer->getProduct();
 
-        $quoteItem->setAdyenPreOrder($product->getAdyenPreOrder()); // set if product is pre order
+        $quoteItem->setAdyenPreOrder((bool)$product->getAdyenPreOrder()); // set if product is pre order
     }
 }
