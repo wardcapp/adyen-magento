6bdc43ca075c640ec6cf4243124c521abd177a56
Sander Mangel <sander@sandermangel.nl>
2016-09-14 15:45:21 +0200

    [TASK] persist adyen_pre_order product attribute in quote and order

---
+++ app/code/community/Adyen/Payment/sql/adyen_setup/mysql4-upgrade-2.7.1.1-2.7.1.2.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Subscription module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2015 H&O E-commerce specialists B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>, H&O E-commerce specialists B.V. <info@h-o.nl>
+ */
+
+/* @var $installer Mage_Sales_Model_Resource_Setup */
+$installer = new Mage_Sales_Model_Resource_Setup('core_setup');
+
+$entities = [
+    'quote_item',
+    'quote_address_item',
+    'order_item'
+];
+
+foreach ($entities as $entity) {
+    $installer->addAttribute($entity, 'adyen_pre_order', [
+        'type'     => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
+        'visible'  => false,
+        'required' => false
+    ]);
+}
+
+$installer->endSetup();
