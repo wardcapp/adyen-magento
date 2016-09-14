6d685aea665529b3c64560491cbc8064d4fcaa00
Rik ter Beek <rikterbeek@me.com>
2016-09-14 15:31:29 +0200

    #670 add pre-order attribute

---
+++ app/code/community/Adyen/Payment/sql/adyen_setup/mysql4-upgrade-2.7.1-2.7.1.1.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Subscription module (https://www.adyen.com/)
+ *
+ * Copyright (c) 2015 H&O E-commerce specialists B.V. (http://www.h-o.nl/)
+ * See LICENSE.txt for license details.
+ *
+ * Author: Adyen <magento@adyen.com>, H&O E-commerce specialists B.V. <info@h-o.nl>
+ */
+
+/* @var $installer Mage_Catalog_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+/** @var Magento_Db_Adapter_Pdo_Mysql $connection */
+$connection = $installer->getConnection();
+
+$installer->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'adyen_pre_order', array(
+        'group'         => 'Adyen',
+        'backend'       => 'catalog/product_attribute_backend_boolean',
+        'frontend'      => '',
+        'label'         => 'Pre-order',
+        'input'         => 'select',
+        'class'         => '',
+        'source'        => 'eav/entity_attribute_source_boolean',
+        'global'        => true,
+        'visible'       => true,
+        'required'      => false,
+        'user_defined'  => false,
+        'default'       => '',
+        'apply_to'      => '',
+        'is_configurable'  => 0,
+        'visible_on_front' => false
+));
+
+$installer->endSetup();
