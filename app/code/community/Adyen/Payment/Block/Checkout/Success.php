ec171e17c484f909ade15e6065433fd31e8d79c1
Leandro F. L <leandro.luvisotto@empiricus.com.br>
2017-11-17 16:54:19 -0200

    Multibanco payment method

--- app/code/community/Adyen/Payment/Block/Checkout/Success.php
+++ app/code/community/Adyen/Payment/Block/Checkout/Success.php
@@ -37,8 +37,9 @@ class Adyen_Payment_Block_Checkout_Success extends Mage_Checkout_Block_Onepage_S
 	{
 		$this->order = Mage::getModel('sales/order')->loadByIncrementId($this->getOrderId());
 		
-		if ($this->order->getPayment() && $this->order->getPayment()->getMethod() == "adyen_boleto")
-			return true;
+		if ($this->order->getPayment() && $this->order->getPayment()->getMethod() == "adyen_boleto") {
+            return true;
+        }
 		
 		return false;
 	}
@@ -51,13 +52,50 @@ class Adyen_Payment_Block_Checkout_Success extends Mage_Checkout_Block_Onepage_S
 		$result = "";
 		
 		// if isBoletoPayment is not called first load the order
-		if($this->order == null)
-			$this->order = Mage::getModel('sales/order')->loadByIncrementId($this->getOrderId());
+		if($this->order == null) {
+            $this->order = Mage::getModel('sales/order')->loadByIncrementId($this->getOrderId());
+        }
 				
-		if ($this->order->getPayment()->getMethod() == "adyen_boleto")
-			$result = $this->order->getAdyenBoletoPdf();
+		if ($this->order->getPayment()->getMethod() == "adyen_boleto") {
+            $result = $this->order->getAdyenBoletoPdf();
+        }
 				
 		return $result;
 	}
-		
+
+    /*
+     * check if payment method is multibanco
+     */
+    public function isMultibancoPayment()
+    {
+        $this->order = Mage::getModel('sales/order')->loadByIncrementId($this->getOrderId());
+
+        if ($this->order->getPayment() && $this->order->getPayment()->getMethod() == 'adyen_multibanco') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMultibanco()
+    {
+        $this->order = Mage::getModel('sales/order')->loadByIncrementId($this->getOrderId());
+
+        $additionalInformation = $this->order->getPayment()->getAdditionalInformation();
+
+        $additionalInformation['comprafacil.deadline_date'] = $this->helper('core')->formatDate($this->order->getCreatedAtStoreDate());
+
+        if ($additionalInformation['comprafacil.deadline'] > 0) {
+            $zendDate = new Zend_Date($this->order->getCreatedAtStoreDate());
+
+            $zendDate->addDay($additionalInformation['comprafacil.deadline']);
+
+            $additionalInformation['comprafacil.deadline_date'] = $this->helper('core')->formatDate($zendDate);
+        }
+
+        return $additionalInformation;
+    }
 }
---
+++ app/code/community/Adyen/Payment/Model/Adyen/Multibanco.php
@@ -0,0 +1,67 @@
+<?php
+
+class Adyen_Payment_Model_Adyen_Multibanco extends Adyen_Payment_Model_Adyen_Abstract
+    implements Mage_Payment_Model_Billing_Agreement_MethodInterface
+{
+    protected $_code = 'adyen_multibanco';
+    protected $_formBlockType = 'adyen/form_multibanco';
+    protected $_infoBlockType = 'adyen/info_multibanco';
+    protected $_paymentMethod = 'multibanco';
+
+    /**
+     * Assign data to info model instance
+     *
+     * @param   mixed $data
+     * @return  Mage_Payment_Model_Info
+     */
+    public function assignData($data)
+    {
+        if (!($data instanceof Varien_Object)) {
+            $data = new Varien_Object($data);
+        }
+
+        $info = $this->getInfoInstance();
+
+        $info->setAdditionalInformation('delivery_date', date('Y-m-d\TH:i:s.000\Z'));
+
+        return $this;
+    }
+
+    /**
+     * @param Mage_Sales_Model_Quote|null $quote
+     * @return bool
+     */
+    public function isAvailable($quote = null)
+    {
+        $isAvailable = parent::isAvailable($quote);
+
+        if (!is_null($quote) && $quote->getGrandTotal() == 0) {
+            $isAvailable = false;
+        }
+
+        return $isAvailable;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canCreateAdyenSubscription()
+    {
+        // validate if recurringType is correctly configured
+        $recurringType = $this->_getConfigData('recurringtypes', 'adyen_abstract');
+
+        if ($recurringType == 'RECURRING' || $recurringType == 'ONECLICK,RECURRING') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBillingAgreement()
+    {
+        return true;
+    }
+}
