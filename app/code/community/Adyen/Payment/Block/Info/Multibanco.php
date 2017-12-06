cc2081cf32cca1dcaddc2fdb9b5af4f1640b39b0
Leandro F. L <leandro.luvisotto@empiricus.com.br>
2017-12-06 15:26:02 -0200

    Displaying multibanco payment information

--- app/code/community/Adyen/Payment/Block/Info/Multibanco.php
+++ app/code/community/Adyen/Payment/Block/Info/Multibanco.php
@@ -8,31 +8,4 @@ class Adyen_Payment_Block_Info_Multibanco extends Mage_Payment_Block_Info
 
         $this->setTemplate('adyen/info/multibanco.phtml');
     }
-
-    /**
-     * @return array|null
-     */
-    public function getMultibanco()
-    {
-        $additionalInformation = $this->getInfo()->getAdditionalInformation();
-
-        if (isset($additionalInformation['comprafacil.entity'])) {
-            /** @var Mage_Sales_Model_Order $salesOrder */
-            $salesOrder = Mage::getModel('sales/order')->load($this->getInfo()->getParentId());
-
-            $additionalInformation['comprafacil.deadline_date'] = $this->helper('core')->formatDate($salesOrder->getCreatedAtStoreDate());
-
-            if ($additionalInformation['comprafacil.deadline'] > 0) {
-                $zendDate = new Zend_Date($salesOrder->getCreatedAtStoreDate());
-
-                $zendDate->addDay($additionalInformation['comprafacil.deadline']);
-
-                $additionalInformation['comprafacil.deadline_date'] = $this->helper('core')->formatDate($zendDate);
-            }
-        } else {
-            $additionalInformation = null;
-        }
-
-        return $additionalInformation;
-    }
 }
