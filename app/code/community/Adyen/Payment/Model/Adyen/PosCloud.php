140abe5ae9ea7b106adddfbaeb9d3020ce45e932
Alessio Zampatti <gigendh@gmail.com>
2018-09-19 09:17:02 +0200

    PW-596: Add cloud pos payment method (#933)
    * PW-596: Added new payment method Adyen_PosCloud
    
    * PW-596: Show pos_cloud on backend as well
    
    * PW-596: Removed redirect methods

---
+++ app/code/community/Adyen/Payment/Model/Adyen/PosCloud.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Adyen Payment Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category	Adyen
+ * @package	Adyen_Payment
+ * @copyright	Copyright (c) 2011 Adyen (http://www.adyen.com)
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+/**
+ * @category   Payment Gateway
+ * @package    Adyen_Payment
+ * @author     Adyen
+ * @property   Adyen B.V
+ * @copyright  Copyright (c) 2014 Adyen BV (http://www.adyen.com)
+ */
+class Adyen_Payment_Model_Adyen_PosCloud extends Adyen_Payment_Model_Adyen_Abstract {
+
+    protected $_canUseInternal = false;
+    protected $_code = 'adyen_pos_cloud';
+    protected $_formBlockType = 'adyen/form_posCloud';
+    protected $_infoBlockType = 'adyen/info_posCloud';
+    protected $_paymentMethod = 'pos_cloud';
+    protected $_isInitializeNeeded = true;
+
+    /**
+     * @var GUEST_ID , used when order is placed by guests
+     */
+    const GUEST_ID = 'customer_';
+
+    protected $_paymentMethodType = 'pos_cloud';
+
+    public function getPaymentMethodType() {
+        return $this->_paymentMethodType;
+    }
+
+    /**
+     * @desc Get checkout session namespace
+     *
+     * @return Mage_Checkout_Model_Session
+     */
+    public function getCheckout() {
+        return Mage::getSingleton('checkout/session');
+    }
+
+    /**
+     * @desc Get current quote
+     *
+     * @return Mage_Sales_Model_Quote
+     */
+    public function getQuote() {
+        return $this->getCheckout()->getQuote();
+    }
+
+    public function getOrderPlaceRedirectUrl() {
+        return Mage::getUrl('adyen/process/redirect');
+    }
+
+    public function getFormName() {
+        return "Adyen POS Cloud";
+    }
+
+    /**
+     * Return redirect block type
+     *
+     * @return string
+     */
+    public function getRedirectBlockType() {
+        return $this->_redirectBlockType;
+    }
+
+    public function initialize($paymentAction, $stateObject) {
+        $state = Mage_Sales_Model_Order::STATE_NEW;
+        $stateObject->setState($state);
+        $stateObject->setStatus($this->_getConfigData('order_status'));
+    }
+
+}
